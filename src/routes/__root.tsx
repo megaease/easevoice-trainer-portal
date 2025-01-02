09cef3683352cb1b91bb1371268547c16256cee7
whhjdi <wanghandi95@gamil.com>
2025-01-02 15:32:28 +0800

    feat: add React Query, axios and Sonner

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { QueryClient } from '@tanstack/react-query'
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Toaster } from '@/components/ui/sonner'
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
   component: RootComponent,
 })
 
 function RootComponent() {
   return (
     <React.Fragment>
-      <div>Hello "__root"!</div>
       <Outlet />
+      <Toaster />
+      {import.meta.env.MODE === 'development' && (
+        <>
+          <ReactQueryDevtools buttonPosition='bottom-left' />
+          <TanStackRouterDevtools position='bottom-right' />
+        </>
+      )}
     </React.Fragment>
   )
 }
