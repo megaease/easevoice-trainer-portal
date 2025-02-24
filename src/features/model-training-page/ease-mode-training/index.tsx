e068f6f4525252c89270ce34ec6facd933830f03
whhjdi <wanghandi95@gamil.com>
2025-02-24 09:45:40 +0800

    refactor: add ScrollArea component to enhance layout in EaseModeTraining

--- src/features/model-training-page/ease-mode-training/index.tsx
+++ src/features/model-training-page/ease-mode-training/index.tsx
@@ -1,17 +1,20 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import EaseModeTrainingForm from './ease-mode-training-form'
 
 export default function EaseModeTraining() {
   return (
-    <div className='p-4'>
-      <Card className='w-full'>
-        <CardHeader>
-          <CardTitle>基础模式</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <EaseModeTrainingForm />
-        </CardContent>
-      </Card>
+    <div className='p-4 h-full'>
+      <ScrollArea className='h-full w-full'>
+        <Card className='w-full'>
+          <CardHeader>
+            <CardTitle>基础模式</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <EaseModeTrainingForm />
+          </CardContent>
+        </Card>
+      </ScrollArea>
     </div>
   )
 }
